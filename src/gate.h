9787e445d9e34ebe72b99a7e3bd0f4bbd4ea3c8f
Richard Kwok <richardjkwok@gmail.com>
2017-09-22 02:00:07 -0700

    Add logic Gate class and factory functions for each type of gate

---
+++ src/gate.h
@@ -0,0 +1,56 @@
+#ifndef LG_GATE
+#define LG_GATE
+
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
+
+typedef sf::Vector2f Vec2;
+
+class Gate {
+    
+public:
+
+    Gate(sf::Texture& texture, const int input_count, Vec2* input_position, Vec2 output_position, bool (*logic_operation)(Gate**)) 
+        : sprite(texture)
+        , input_count(input_count)
+        , input_position(input_position)
+        , output_position(output_position)
+        , logic_operation(logic_operation)
+    {
+        input = new Gate*[input_count];    
+    }
+
+    ~Gate() 
+    {
+        delete input;
+        delete input_position;
+    }
+
+    bool operator()() 
+    { 
+        return logic_operation(input); 
+    }   
+
+    bool (*logic_operation)(Gate**);
+
+    Gate**      input;
+    Vec2*       input_position;
+    int         input_count;
+    Vec2        output_position;
+    sf::Sprite  sprite;
+};
+
+bool OR(Gate** input) {
+    return (*input ? (**input)() : 0) || (*(input + 1) ? (**(input + 1))() : 0);
+}
+
+bool AND(Gate** input) {
+    return (*input ? (**input)() : 0) && (*(input + 1) ? (**(input + 1))() : 0);
+}
+
+bool NOT(Gate** input) {
+    return !(*input ? (**input)() : 0);
+}
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,11 @@
 #include <stdlib.h>
 // floor
 #include <math.h>
+// function
+#include <functional>
 
 #include "util.h"
+#include "gate.h"
 
 typedef sf::Vector2f Vec2;
 
@@ -24,6 +27,27 @@ int main() {
     // Create a view that will represent the transformed and scaled view of the scene
     sf::View scene_view = window_view;
 
+    // Load textures
+    sf::Texture or_texture;
+    sf::Texture and_texture;
+    sf::Texture not_texture;
+    
+    or_texture.loadFromFile("resources/or.png");
+    and_texture.loadFromFile("resources/and.png");
+    not_texture.loadFromFile("resources/not.png");
+
+    // Make some factory functions for creating certain types of logic gates
+    auto or_factory = [&or_texture](){ return new Gate(or_texture, 2, new Vec2[2]{ Vec2(0, 15), Vec2(0, 75) }, Vec2(89, 45), OR); };
+    auto and_factory = [&and_texture](){ return new Gate(and_texture, 2, new Vec2[2]{ Vec2(0, 15), Vec2(0, 75) }, Vec2(89, 45), AND); };
+    auto not_factory = [&not_texture](){ return new Gate(not_texture, 1, new Vec2[1]{ Vec2(0, 45) }, Vec2(89, 45), NOT); };
+
+    // Put factory functions in an iterable container for easy UI access
+    std::vector<std::function<Gate*()> > factories { or_factory, and_factory, not_factory};
+    auto factory = factories.begin();
+
+    // Declare gate tracking vector
+    std::vector<Gate*> gates;
+
     // Instantiate our input struct to hold the inputs for each tick
     Input input;
 
@@ -33,6 +57,9 @@ int main() {
     sf::RectangleShape cursor(Vec2(tile_width, tile_width));
     cursor.setOutlineThickness(0);
     cursor.setFillColor(sf::Color(0, 255, 0, 155));
+
+    // Buffer to allow two gates to be selected for wiring
+    Gate* wiring_gate = nullptr;
 
     // Initialize loop timer
     sf::Clock timer;
@@ -61,7 +88,48 @@ int main() {
             window.close();
         }
 
+        // If the user hits tab, switch the kind of logic gate that is being generated
+        if (input.keyReleased(sf::Keyboard::Tab)) {
+            factory++;
+            if (factory == factories.end()) {
+                factory = factories.begin();
+            }
+        }
+
+        // Update cursor position and snap to grid
         cursor.setPosition(floor(input.scene_mouse.x/tile_width)*tile_width, floor(input.scene_mouse.y/tile_width)*tile_width);
+
+        // Create new gate at cursor position if user clicks
+        if (input.lmb_released && !wiring_gate) {
+            gates.push_back((*factory)());
+            gates[gates.size() - 1]->sprite.setPosition(cursor.getPosition());
+        }
+
+        if (input.rmb_released && !wiring_gate) {
+            for (auto each : gates) {
+                sf::Vector2f output_position = each->sprite.getPosition() + each->output_position;
+                if (floor(input.scene_mouse.x/tile_width) == floor(output_position.x/tile_width) &&
+                    floor(input.scene_mouse.y/tile_width) == floor(output_position.y/tile_width)) 
+                {
+                    wiring_gate = each;
+                    cursor.setFillColor(sf::Color(255, 0, 0, 155));
+                }
+            }
+        }
+        else if (input.rmb_released && wiring_gate) {
+            for (auto each : gates) {
+                for (int i = 0; i < each->input_count; i++) {
+                    sf::Vector2f input_position = each->sprite.getPosition() + *(each->input_position + i);
+                    if (floor(input.scene_mouse.x/tile_width) == floor(input_position.x/tile_width) &&
+                        floor(input.scene_mouse.y/tile_width) == floor(input_position.y/tile_width)) 
+                    {
+                        *(each->input + i) = wiring_gate;
+                        wiring_gate = nullptr;
+                        cursor.setFillColor(sf::Color(0, 255, 0, 155));
+                    }
+                }
+            }
+        }
 
         //////////////////////////////////////////
         // Draw
@@ -69,6 +137,9 @@ int main() {
 
         window.clear();
 
+        for (auto each : gates) {
+            window.draw(each->sprite);
+        }
         window.draw(cursor);
 
         window.display();
