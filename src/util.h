e81e6fc8503e1959207d27d703029bb92afa06ae
Richard Kwok <richardjkwok@gmail.com>
2017-09-22 09:56:45 -0700

    Add functions to make sf::RectangleShape lines and sf::CircleShape dots

--- src/util.h
+++ src/util.h
@@ -5,6 +5,8 @@
 #include "SFML/Window.hpp"
 #include "SFML/Graphics.hpp"
 
+// atan2
+#include <math.h>
 #include <sstream>
 
 template <typename T>
@@ -12,6 +14,35 @@ std::string asString(T number) {
     std::stringstream sstream;
     sstream << number;
     return sstream.str();
+}
+
+constexpr double pi = std::atan(1)*4;
+
+sf::RectangleShape makeLine(const sf::Vector2f& a, const sf::Vector2f& b, const sf::Color& color) {
+
+    static const float thickness = 5.0f;
+
+    float h = hypot(b.x - a.x, b.y - a.y);
+
+    sf::RectangleShape line(sf::Vector2f(h, thickness));
+    line.setOutlineThickness(0);
+    line.setFillColor(color);
+    line.setOrigin(0.0f, thickness/2.0f);
+    line.setRotation(atan2(b.y - a.y, b.x - a.x)*180.0f/pi);
+    line.setPosition(a);
+
+    return line;
+}
+
+sf::CircleShape makeCircle(const sf::Vector2f a, float radius, const sf::Color& color) {
+
+    sf::CircleShape circle(radius);
+    circle.setOutlineThickness(0);
+    circle.setFillColor(color);
+    circle.setOrigin(radius, radius);
+    circle.setPosition(a);
+
+    return circle;
 }
 
 struct Input {
